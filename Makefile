bf018a8c6aa98f96feb2314a22752dcddfe08413
Sam Johnson <sambjohnson94@gmail.com>
2023-02-22 02:21:48 -0800

    working hello world (hey girl) w/ binfmt

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@ default:
 	riscv64-linux-gnu-as hello.s -o hello.o
 	riscv64-linux-gnu-gcc -o hello hello.o -nostdlib -static
 
+run:
+	qemu-riscv64 hello
+
 clean:
 	rm -rf hello hello.o
--- hello.s
+++ hello.s
@@ -1,5 +1,16 @@
 .global _start
 
 _start:
+	la a1, helloworld	# *buf
+	addi a2, zero, 13	# 13 byte size
+	addi a7, zero, 64	# syscall for write
+	ecall			# call the set of a7 func, w/ args a0 thru a2 
+	addi a0, zero, 0	# linux fd
+
+
 	addi a7, zero, 93	# linux syscall for exit
-	addi a0, zero, 13
+	ecall			
+
+helloworld:
+	.ascii "Hey girl! ;3\n"
+
